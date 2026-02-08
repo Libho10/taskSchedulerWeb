550597d83206c87c4d5df02558bd1efacfc851e4
Libho10 <Libhomato@gmail.com>
2026-02-08 18:58:05 +0200

    Improve comments in Program.cs
    Updated comments for clarity in Program.cs

--- taskSchedulerWeb/Program.cs
+++ taskSchedulerWeb/Program.cs
@@ -1,15 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Added services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuring the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. need to change incase. 
     app.UseHsts();
 }
 
